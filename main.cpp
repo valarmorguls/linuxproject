f18c2a460b6d01ba913457cf72b6254c023c2bab
xupeng <1158091336@qq.com>
2023-02-02 13:20:06 +0800

    first example

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: main.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Thu 02 Feb 2023 01:19:06 PM CST
+ ************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "hello world!" << endl;
+    return 0;
+}
